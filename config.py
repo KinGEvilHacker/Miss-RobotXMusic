c78706e4d82b20d35642ba4b2b0a52c8ba3955bf
KinGEvilHacker <103206714+KinGEvilHacker@users.noreply.github.com>
2023-08-06 13:35:56 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -7,26 +7,26 @@ from pyrogram import filters
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("28733143"))
+API_HASH = getenv("f7bbd29cf8ba336237046dbecfeab519")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("6458576383:AAH8x4HMFI-bOjm_jv_sqJlyjm0cZZEIY1Q")
 
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "Tᴀsʜʀɪ Rᴏʙᴏᴛ Mᴜsɪᴄ")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://hnyx:wywyw2@cluster0.9dxlslv.mongodb.net/?retryWrites=true&w=majority")
+LOG_GROUP_ID = int(getenv("-1001989867442"))
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "ᴍɪss 𝐑σႦσ𝐓 ✗ ᴍᴜꜱɪᴄ")
 
-OWNER_ID = list(map(int, getenv("OWNER_ID", "5105943340").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "1138802391").split()))
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Tashri2342/TashriMusic")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/KinGEvilHacker/TashriMusic")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Tashribots2342")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/TeamTashri")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/miss_x_network")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/themortalclan")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
@@ -76,11 +76,11 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/8d87e070888943214162a.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://graph.org/file/83ec2821355336e588265.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
-    "https://te.legra.ph/file/773a736d83a0166aa21c3.jpg",
+    "https://graph.org/file/83ec2821355336e588265.jpg",
 )
 
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
@@ -130,9 +130,9 @@ if UPSTREAM_REPO:
 if PING_IMG_URL:
     if PING_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", PING_IMG_URL):
-            PING_IMG_URL = "https://te.legra.ph/file/773a736d83a0166aa21c3.jpg"
+            PING_IMG_URL = "https://graph.org/file/c4dc510f7532f842f59bb.jpg"
 
 if START_IMG_URL:
     if START_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", START_IMG_URL):
-            START_IMG_URL = "https://te.legra.ph/file/8d87e070888943214162a.jpg"
+            START_IMG_URL = "https://graph.org/file/c4dc510f7532f842f59bb.jpg"
