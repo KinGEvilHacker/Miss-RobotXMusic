5b111afbff449b43efefed192db53d4cb012db79
KinGEvilHacker <103206714+KinGEvilHacker@users.noreply.github.com>
2023-08-06 13:36:52 +0530

    Add files via upload

--- strings/__init__.py
+++ strings/__init__.py
@@ -1,40 +1,37 @@
-import os
-from typing import List
+from TashriX.core.bot import TashriXBot
+from TashriX.core.dir import dirr
+from TashriX.core.git import git
+from TashriX.core.userbot import Userbot
+from TashriX.misc import dbb, heroku, sudo
+from aiohttp import ClientSession
 
-import yaml
-
-languages = {}
-commands = {}
-
-
-def get_command(value: str) -> List:
-    return commands["command"][value]
+from .logging import LOGGER
 
 
-def get_string(lang: str):
-    return languages[lang]
+dirr()
+
+git()
+
+dbb()
+
+heroku()
+
+sudo()
+
+# Clients
+app = TashriXBot()
+
+userbot = Userbot()
 
 
-for filename in os.listdir(r"./strings"):
-    if filename.endswith(".yml"):
-        language_name = filename[:-4]
-        commands[language_name] = yaml.safe_load(
-            open(r"./strings/" + filename, encoding="utf8")
-        )
+from .platforms import *
 
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
 
-for filename in os.listdir(r"./strings/langs/"):
-    if "en" not in languages:
-        languages["en"] = yaml.safe_load(
-            open(r"./strings/langs/en.yml", encoding="utf8")
-        )
-    if filename.endswith(".yml"):
-        language_name = filename[:-4]
-        if language_name == "en":
-            continue
-        languages[language_name] = yaml.safe_load(
-            open(r"./strings/langs/" + filename, encoding="utf8")
-        )
-        for item in languages["en"]:
-            if item not in languages[language_name]:
-                languages[language_name][item] = languages["en"][item]
+aiohttpsession = ClientSession()
